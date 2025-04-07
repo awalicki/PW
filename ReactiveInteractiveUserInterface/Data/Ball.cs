e27ec2657bf55b334c84aa2535b3d3315bb6e727
Adam Walicki <awalicki04@gmail.com>
2025-04-07 20:52:41 +0200

    Dodanie granic i usunięcie losowości ruchu

--- ReactiveInteractiveUserInterface/Data/Ball.cs
+++ ReactiveInteractiveUserInterface/Data/Ball.cs
@@ -1,50 +1,62 @@
-﻿//____________________________________________________________________________________________________________________________________
-//
-//  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
-//
-//  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
-//
-//  https://github.com/mpostol/TP/discussions/182
-//
-//_____________________________________________________________________________________________________________________________________
-
-namespace TP.ConcurrentProgramming.Data
+﻿namespace TP.ConcurrentProgramming.Data
 {
-  internal class Ball : IBall
-  {
-    #region ctor
-
-    internal Ball(Vector initialPosition, Vector initialVelocity)
+    internal class Ball : IBall
     {
-      Position = initialPosition;
-      Velocity = initialVelocity;
+        #region ctor
+
+        internal Ball(Vector initialPosition, Vector initialVelocity)
+        {
+            Position = initialPosition;
+            Velocity = initialVelocity;
+        }
+
+        #endregion ctor
+
+        #region IBall
+
+        public event EventHandler<IVector>? NewPositionNotification;
+
+        public IVector Velocity { get; set; }
+
+        #endregion IBall
+
+        #region private
+
+        private Vector Position;
+
+        // Pomocnicze - żeby kod był czytelniejszy
+        private Vector InternalVelocity => (Vector)Velocity;
+
+        private void RaiseNewPositionChangeNotification()
+        {
+            NewPositionNotification?.Invoke(this, Position);
+        }
+
+        internal void MoveWithBounds(Vector delta, double minX, double maxX, double minY, double maxY)
+        {
+            double newX = Position.x + delta.x;
+            double newY = Position.y + delta.y;
+
+            if (newX < minX || newX > maxX)
+                Velocity = new Vector(-InternalVelocity.x, InternalVelocity.y);
+
+            if (newY < minY || newY > maxY)
+                Velocity = new Vector(InternalVelocity.x, -InternalVelocity.y);
+
+            Position = new Vector(
+                Math.Clamp(newX, minX, maxX),
+                Math.Clamp(newY, minY, maxY)
+            );
+
+            RaiseNewPositionChangeNotification();
+        }
+
+        internal void Move(Vector delta)
+        {
+            Position = new Vector(Position.x + delta.x, Position.y + delta.y);
+            RaiseNewPositionChangeNotification();
+        }
+
+        #endregion private
     }
-
-    #endregion ctor
-
-    #region IBall
-
-    public event EventHandler<IVector>? NewPositionNotification;
-
-    public IVector Velocity { get; set; }
-
-    #endregion IBall
-
-    #region private
-
-    private Vector Position;
-
-    private void RaiseNewPositionChangeNotification()
-    {
-      NewPositionNotification?.Invoke(this, Position);
-    }
-
-    internal void Move(Vector delta)
-    {
-      Position = new Vector(Position.x + delta.x, Position.y + delta.y);
-      RaiseNewPositionChangeNotification();
-    }
-
-    #endregion private
-  }
 }
