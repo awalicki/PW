8c812cf2ec44441b5abfabff1d67d1f3608d118b
Adam Walicki <awalicki04@gmail.com>
2025-04-11 23:12:38 +0200

    Dodanie Interaktywnego GUI

--- ReactiveInteractiveUserInterface/GraphicalUserInterface/MainWindow.xaml.cs
+++ ReactiveInteractiveUserInterface/GraphicalUserInterface/MainWindow.xaml.cs
@@ -1,42 +1,35 @@
-﻿//__________________________________________________________________________________________
-//
-//  Copyright 2024 Mariusz Postol LODZ POLAND.
-//
-//  To be in touch join the community by pressing the `Watch` button and to get started
-//  comment using the discussion panel at
-//  https://github.com/mpostol/TP/discussions/182
-//__________________________________________________________________________________________
-
-using System;
+﻿using System;
 using System.Windows;
 using TP.ConcurrentProgramming.Presentation.ViewModel;
 
 namespace TP.ConcurrentProgramming.PresentationView
 {
-  /// <summary>
-  /// View implementation
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-      Random random = new Random();
-      InitializeComponent();
-      MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
-      double screenWidth = SystemParameters.PrimaryScreenWidth;
-      double screenHeight = SystemParameters.PrimaryScreenHeight;
-      viewModel.Start(random.Next(5, 10));
-    }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-    /// <summary>
-    /// Raises the <seealso cref="System.Windows.Window.Closed"/> event.
-    /// </summary>
-    /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
-    protected override void OnClosed(EventArgs e)
-    {
-      if (DataContext is MainWindowViewModel viewModel)
-        viewModel.Dispose();
-      base.OnClosed(e);
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(BallsCountTextBox.Text, out int ballCount) && ballCount >= 1 && ballCount <= 15)
+            {
+                ErrorTextBlock.Text = ""; // Czyścimy komunikat błędu
+                MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
+                viewModel.Start(ballCount);
+            }
+            else
+            {
+                ErrorTextBlock.Text = "Podaj liczbę z przedziału 1-15.";
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if (DataContext is MainWindowViewModel viewModel)
+                viewModel.Dispose();
+            base.OnClosed(e);
+        }
     }
-  }
 }
