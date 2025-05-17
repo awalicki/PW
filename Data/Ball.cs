94e750abfe66549e0084a32313eaa7bd4dffd5e3
251652 Tomecki Remigiusz <251652@edu.p.lodz.pl>
2025-05-17 17:53:06 +0200

    Zmiana Velocity

--- Data/Ball.cs
+++ Data/Ball.cs
@@ -19,33 +19,16 @@ namespace TP.ConcurrentProgramming.Data
         internal Ball(Vector initialPosition, Vector initialVelocity, double weight)
         {
             Position = initialPosition;
-            _velocity = initialVelocity;
+            Velocity = initialVelocity;
             BallRadius = 10;
             Weight = weight;
             _cancellationTokenSource = new CancellationTokenSource();
         }
 
         public event EventHandler<IVector>? NewPositionNotification;
-        private IVector _velocity;
         private readonly object _velocityLock = new object();
 
-        public IVector Velocity
-        {
-            get
-            {
-                lock (_velocityLock)
-                {
-                    return _velocity;
-                }
-            }
-            set
-            {
-                lock (_velocityLock)
-                {
-                    _velocity = value;
-                }
-            }
-        }
+        public IVector Velocity { get; set;}
 
         public double Weight { get; }
         public double BallRadius { get; }
@@ -84,7 +67,7 @@ namespace TP.ConcurrentProgramming.Data
                     IVector currentVelocity;
                     lock (_velocityLock)
                     {
-                        currentVelocity = _velocity;
+                        currentVelocity = Velocity;
                     }
 
                     Vector velocityVector = (Vector)currentVelocity;
